02d7501d04915d7b26205344b1ea3d33447ebeed
chanchal16 <rajputchanchal17@gmail.com>
2022-06-28 23:46:42 +0530

    Create footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer className="bg-zinc-800 text-gray-300 p-2 text-center w-full">
+        Made by <a href="https://github.com/chanchal16" target="_blank" className="text-teal-300 hover:text-teal-600">Chanchal</a>.
+    </footer>
+  )
+}

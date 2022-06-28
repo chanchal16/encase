12503a27e149ef1c99de94e611804962318c063e
chanchal16 <rajputchanchal17@gmail.com>
2022-06-28 23:47:19 +0530

    Create about page

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div className='template-intro'>
+      <h3 className='text-3xl text-center font-bold mb-3 text-zinc-600 pt-24'>
+        About page
+      </h3>
+      <p className='text-lg text-center text-zinc-600'>This is just a demo to show how you can add new routes to the app.</p>
+    </div>
+  )
+}

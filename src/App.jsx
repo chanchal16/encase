b681123d56c6e6bcf77873a73f5b5e5ba53429ea
chanchal16 <rajputchanchal17@gmail.com>
2022-06-28 23:44:36 +0530

    Update - add routes

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./tailwind.css";
+import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
+
+const App = () =>{
+    return(
+        <div className="App">
+            <Navbar/>
+            <main className="container mx-auto">
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/about" element={<About/>}/>
+                </Routes>
+            </main>
+            <Footer/>
+        </div>
+    )
+}
+export default App
